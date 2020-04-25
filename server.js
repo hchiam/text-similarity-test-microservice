df8e43ac942870299819bdfb27d68fcc24f37ed4
Glitch (text-similarity-test-microservice) <none>
2020-04-25 04:29:14 +0000

    got the file reads to work synchronously

--- server.js
+++ server.js
@@ -57,17 +57,10 @@ app.route('/:coglangSentence').get(function(req, res, next) {
       return; // exit early
     }
     
-    // // TODO: go through each embedding
-    // for (let embedding of data) {
-    // }
+    // just get closest 5 matches to first missing word:
+    const mostSimilarWords = await useModel(missingWords[0]);
     
-    // TODO: just get closest match to first missing word
-    
-    // for now, just check closeness of match of first missing word with one word in the vocab
-    const similarityPercent = await useModel('hello', missingWords[0]);
-    console.log(8, similarityPercent)
-    
-    outputData.suggestions = similarityPercent || 0;
+    outputData.suggestions = mostSimilarWords;
     
     console.log(outputData);
 
@@ -149,46 +142,72 @@ function getShortForm(cog) {
 
 // Tensorflow.js stuff:
 
-async function useModel(sentence1, sentence2) {
-  console.log(1, sentence1, sentence2);
+async function useModel(inputWord) {
   // uses Universal Sentence Encoder (U.S.E.):
-  return await use.load().then(async (model) => {
-    console.log(2, 'inside use.load');
-    const similarityFraction = await embedSentences(model, sentence1, sentence2);
-    console.log(7, 'similarityFraction ' + similarityFraction)
-    return Math.round(similarityFraction * 100 * 100) / 100 + '%';
+  const mostSimilarWords = await use.load().then(async (model) => {
+    return await getEmbedding(model, inputWord);
   });
+  return mostSimilarWords;
 }
 
-async function embedSentences(model, sentence1, sentence2) {
-  const sentences = [sentence1, sentence2];
-  return await model.embed(sentences).then(async (embeddings) => {
-    console.log(3, 'inside model.embed');
-    const embeds = await embeddings.arraySync();
-    console.log(4, 'did arraySync after got embeddings');
-    const sentence1Embedding = embeds[0];
-    const sentence2Embedding = embeds[1];
-    const similarityPercent = await getSimilarityPercent(sentence1Embedding, sentence2Embedding);
-    console.log(6, 'similarityPercent ' + similarityPercent);
-    return similarityPercent;
-  });
+function getEmbedding(model, inputWord) {
+  return model
+    .embed([inputWord])
+    .then((inputEmbeddings) => {
+      const embeds = inputEmbeddings.arraySync();
+      const wordEmbedding = embeds[0];
+
+      const embedsData = readFile("embeddings.txt");
+      const lines = embedsData.split("\n");
+      if (lines.length === 0) return []; // exit if no data
+
+      const similarities = getAllSimilarityScores(wordEmbedding, embedsData);
+      const top5 = getTop5Similarities(similarities);
+
+      const englishData = readFile("out_english.txt");
+      const mostSimilarWords = getMostSimilarWordsFromFile(top5, englishData);
+      // console.log(mostSimilarWords);
+      return mostSimilarWords;
+    })
+    .catch((err) => {
+      console.log(err);
+      return [];
+    });
 }
 
-async function getSimilarityPercent(embed1, embed2) {
-  const similarity = await cosineSimilarity(embed1, embed2);
-  console.log(5, 'got similarity ' + similarity);
+function readFile(filePath) {
+  return fs.readFileSync(filePath, "utf8");
+}
+
+function getAllSimilarityScores(wordEmbedding, data) {
+  const lines = data.split("\n");
+  if (lines.length === 0) return []; // exit if no data
+  const similarities = []; // TODO: use a max heap instead? Reference: https://github.com/hchiam/learning-google-closure-library/blob/master/goog-closure-example.js
+  for (let index = 0; index < lines.length; index++) {
+    const line = lines[index];
+    const referenceEmbedding = line.split(",").map((n) => Number(n));
+    if (referenceEmbedding.length === 0) continue; // skip empty line
+    const similarity = getSimilarityPercent(wordEmbedding, referenceEmbedding);
+    similarities.push({ similarity, index });
+  }
+  // console.log("similarities.length: " + similarities.length);
+  return similarities;
+}
+
+function getSimilarityPercent(wordEmbedding, referenceEmbedding) {
+  const similarity = cosineSimilarity(wordEmbedding, referenceEmbedding);
   // cosine similarity -> % when doing text comparison, since cannot have -ve term frequencies: https://en.wikipedia.org/wiki/Cosine_similarity
   return similarity;
 }
 
-async function cosineSimilarity(a, b) {
+function cosineSimilarity(a, b) {
   // https://towardsdatascience.com/how-to-build-a-textual-similarity-analysis-web-app-aa3139d4fb71
 
-  const magnitudeA = await Math.sqrt(dotProduct(a, a));
-  const magnitudeB = await Math.sqrt(dotProduct(b, b));
+  const magnitudeA = Math.sqrt(dotProduct(a, a));
+  const magnitudeB = Math.sqrt(dotProduct(b, b));
   if (magnitudeA && magnitudeB) {
     // https://towardsdatascience.com/how-to-measure-distances-in-machine-learning-13a396aa34ce
-    return await dotProduct(a, b) / (magnitudeA * magnitudeB);
+    return dotProduct(a, b) / (magnitudeA * magnitudeB);
   } else {
     return 0;
   }
@@ -200,4 +219,26 @@ function dotProduct(a, b) {
     sum += a[i] * b[i];
   }
   return sum;
+}
+
+function getTop5Similarities(similarities) {
+  return similarities
+    .sort(function descending(a, b) {
+      return b.similarity - a.similarity;
+    })
+    .slice(0, 5);
+  // console.log("top 5 similar indices: " + top5.map((x) => x.index));
+  // console.log("top 5 similarities: " + top5.map((x) => x.similarity));
+}
+
+function getMostSimilarWordsFromFile(top5, data) {
+  const mostSimilarWords = [];
+  const lines = data.split("\n");
+  top5.forEach((similarity) => {
+    const index = similarity.index;
+    const word = lines[index];
+    mostSimilarWords.push(word);
+  });
+  // console.log("most similar words: " + mostSimilarWords);
+  return mostSimilarWords;
 }
